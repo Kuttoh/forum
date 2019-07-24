3bd3975b3d450431034905b0b56d120fd5503097
Kuttoh <kuttohisaac@gmail.com>
2019-07-24 12:48:37 +0300

    refactor

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -15,7 +15,7 @@
                             <div class="form-group">
                                 <label for="channel_id">Choose a channel:</label>
                                 <select name="channel_id" id="channel_id" class="form-control" required>
-                                    <option value="">Choose One bro...</option>
+                                    <option value="">Choose One...</option>
                                     @foreach($channels as $channel)
                                         <option value="{{ $channel->id}}" {{ old('channel_id') == $channel->id ? 'selected': '' }}>
                                             {{ $channel->name }}
