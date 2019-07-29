0e3e6006453778ccf86425a6368a3ab325b92b1e
vagrant <vagrant@homestead>
2019-07-29 10:11:18 +0000

    reduce queries from 56 to two

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Channel;
+use Illuminate\Filesystem\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +26,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \View::composer('*', function ($view) {
-            $view->with('channels', Channel::all());
+            $channels = \Cache::rememberForever('channels', function (){
+                return Channel::all();
+            });
+
+            $view->with('channels', $channels);
         });
     }
 }
